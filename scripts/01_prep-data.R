03e4aca56657aae75d0b84aa6833d6d95d82e865
Fisheries FWL Wildlife <FWL@FWLNR334-PD.anr.msu.edu>
2018-05-14 12:01:01 -0400

    adding date filtering and median calculations

--- scripts/01_prep-data.R
+++ scripts/01_prep-data.R
@@ -11,23 +11,61 @@ lg <- lagosne_load("1.087.1")
 
 # ----select_epi_nutr_variables ----
 # variables: TP, TN, chlorophyll, color(true), Secchi
-
+epi_nutr <- lg$epi_nutr
+secchi <- lg$secchi
 
 # ----filter_to_common_time_period----
-# time period: 1980 - 2011
+# time period: 1980 - 2011, using samples collected between Jun 15 and Sep 15 (stratification period)
+# define time parameters
+first_year <- 1980
+last_year <- 2010
+first_day <- '0615' #i.e., '0615' for Jun 15
+last_day <- '0915'
 
+# convert sampledate to date format and create "monthday" column to filter by day
+epi_nutr$sampledate <- as.Date(epi_nutr$sampledate, format="%m/%d/%Y")
+secchi$sampledate <- as.Date(secchi$sampledate, format="%m/%d/%Y")
+
+epi_nutr$monthday <- format(epi_nutr$sampledate, format="%m%d")
+secchi$monthday <- format(secchi$sampledate, format="%m%d")
+
+# subset by sample date and year cutoffs specified above
+epi_nutr_subset <- epi_nutr[epi_nutr$monthday >= first_day & epi_nutr$monthday <= last_day,]
+epi_nutr_subset <- subset(epi_nutr_subset, sampleyear >= first_year & sampleyear <= last_year)
+
+secchi_subset <- secchi[secchi$monthday >= first_day & secchi$monthday <= last_day,]
+secchi_subset <- subset(secchi_subset, sampleyear >= first_year & sampleyear <= last_year)
 
 # ----calculate_tn----
 # from componenent variables
 
 
 # ----calculate_medians ----
-# median of each summer period
-# median of the summer periods
-# number of years (summer periods)
 
+secchi_subset_medians <- secchi_subset %>% 
+  group_by(lagoslakeid, sampleyear) %>%
+  summarise(nSamples_secchi = n(), secchi=median(secchi))
 
-# ----pull_geo_predictors ----
+epi_nutr_subset_medians <- epi_nutr_subset %>% 
+  group_by(lagoslakeid, sampleyear) %>%
+  summarise(TP=median(tp), chla=median(chla), color=median(colort), TN=median(tn))
+
+# get number of samples for non-Secchi variables
+TP_counts <- aggregate(epi_nutr_subset$tp, by=list(epi_nutr_subset$lagoslakeid, epi_nutr_subset$sampleyear), 
+                       FUN='length')
+colnames(TP_counts) <- c('lagoslakeid', 'sampleyear', 'nSamples_TP')
+
+TN_counts <- aggregate(epi_nutr_subset$tn, by=list(epi_nutr_subset$lagoslakeid, epi_nutr_subset$sampleyear), 
+                       FUN='length')
+colnames(TN_counts) <- c('lagoslakeid', 'sampleyear', 'nSamples_TN')
+color_counts <- aggregate(epi_nutr_subset$colort, by=list(epi_nutr_subset$lagoslakeid, epi_nutr_subset$sampleyear), 
+                          FUN='length')
+colnames(color_counts) <- c('lagoslakeid', 'sampleyear', 'nSamples_color')
+chla_counts <- aggregate(epi_nutr_subset$chla, by=list(epi_nutr_subset$lagoslakeid, epi_nutr_subset$sampleyear), 
+                         FUN='length')
+colnames(chla_counts) <- c('lagoslakeid', 'sampleyear', 'nSamples_chla')
+
+# ----pull_geo_predictors (for all lakes)----
 # max depth
 # lulc
 # hydrology
