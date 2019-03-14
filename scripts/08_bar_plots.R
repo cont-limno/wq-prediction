7a906e085b9542898679580bfceabc0badd424a9
jsta <stachel2@msu.edu>
2019-03-14 15:54:25 -0400

    stash bar plot protoypes

--- scripts/08_bar_plots.R
+++ scripts/08_bar_plots.R
@@ -2,6 +2,25 @@ library(readxl)
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(janitor)
+
+bar_plot_clean <- function(raw){
+  random_lines <- raw %>%
+    filter(set %in% c("random25", "random75")) %>%
+    tidyr::spread(set, value)
+
+  clean <- raw %>%
+    filter(!(set %in% c("random25", "random75"))) %>%
+    mutate(set = factor(set, levels = c("cluster_strat75", "hu4_strat",
+                                        "cluster_random50", "hu4_random",
+                                        "hu4_ago"))) %>%
+    left_join(random_lines) %>%
+    mutate(variable = factor(variable, levels = c("TP", "TN", "Chla", "secchi"))) %>%
+    mutate(polation = case_when(set %in% c("cluster_strat75", "hu4_strat") ~ "interpolation",
+                                TRUE ~ "extrapolation"))
+
+  clean
+}
 
 # ---- rmse ----
 raw           <- readxl::read_excel(
@@ -10,25 +29,70 @@ raw           <- readxl::read_excel(
 names(raw)[1] <- "variable"
 raw <- tidyr::gather(raw, key = set, value = value, -variable)
 
-clean <- raw %>%
-  filter(!(set %in% c("random25", "random75"))) %>%
-  mutate(set = factor(set, levels = c("cluster_strat75", "hu4_strat",
-                                         "cluster_random50", "hu4_random",
-                                         "hu4_ago")))
+clean <- bar_plot_clean(raw)
 
 ggplot(data = clean) +
-  geom_segment(aes(xend = set, yend = 0, x = set, y = value),
+  geom_segment(aes(xend = set, yend = 0, x = set, y = value, color = polation),
                lineend = "butt", size = 8) +
-  # geom_hline() +
-  # ylim(0.3, 1) +
+  geom_hline(aes(yintercept = random25), linetype = "dashed") +
+  geom_hline(aes(yintercept = random75)) +
   facet_wrap(~variable) +
-  theme(axis.text.x = element_text(angle = 90))
-
-
-
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90),
+        legend.title=element_blank()) +
+  ylab("RMSE") + xlab("")
 
 # ---- median_errors ----
 
+raw           <- readxl::read_excel(
+  "data/bar_plot_data/median_error_03082019Qi.xlsx",
+  sheet = "conditional")[,1:4] %>%
+  setNames(c("variable", "set", "error_relative", "error_nonrelative")) %>%
+  janitor::remove_empty(which = "rows") %>%
+  tidyr::fill(variable, .direction = "down") %>%
+  select(-error_relative) %>%
+  rename(value = error_nonrelative) %>%
+  mutate(set = case_when(set %in% c("cluster_random50_holdout") ~ "cluster_random50",
+                              TRUE ~ set)) %>%
+  mutate(variable = case_when(variable %in% c("chla") ~ "Chla",
+                             TRUE ~ variable))
+
+clean <- bar_plot_clean(raw)
+
+ggplot(data = clean) +
+  geom_segment(aes(xend = set, yend = 0, x = set, y = value, color = polation),
+               lineend = "butt", size = 8) +
+  geom_hline(aes(yintercept = random25), linetype = "dashed") +
+  geom_hline(aes(yintercept = random75)) +
+  facet_wrap(~variable, scales = "free") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90),
+        legend.title=element_blank()) +
+  ylab("MRAE") + xlab("")
 
 # ---- r2 ----
 
+raw           <- readxl::read_excel(
+  "data/bar_plot_data/R_squared_03072019Qi.xlsx",
+  sheet = "conditional")[1:35,1:3] %>%
+  setNames(c("variable", "set", "value")) %>%
+  mutate(value = as.numeric(value)) %>%
+  janitor::remove_empty(which = "rows") %>%
+  tidyr::fill(variable, .direction = "down") %>%
+  mutate(set = case_when(set %in% c("cluster_random50_holdout") ~ "cluster_random50",
+                         TRUE ~ set)) %>%
+  mutate(variable = case_when(variable %in% c("chla") ~ "Chla",
+                              TRUE ~ variable))
+
+clean      <- bar_plot_clean(raw)
+clean$x    <- rep(seq(0.5, 4), each = 5)
+clean$xend <- rep(seq(1.5, 5), each = 5)
+
+ggplot(data = clean, aes(variable, value)) +
+  geom_bar(aes(fill = set), stat = "identity", position = position_dodge2()) +
+  geom_segment(aes(x = x, xend = xend, y = random25, yend = random25),
+               linetype = "dashed") +
+  geom_segment(aes(x = x, xend = xend, y = random75, yend = random75)) +
+  theme_minimal() +
+  theme(legend.title=element_blank()) +
+  ylab("R2") + xlab("")
