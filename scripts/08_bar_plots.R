18ff0ee417b135631841883f18e8ccfe1cf4df8c
jsta <stachel2@msu.edu>
2019-03-18 21:07:51 -0400

    parse subscript labels

--- scripts/08_bar_plots.R
+++ scripts/08_bar_plots.R
@@ -3,6 +3,34 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(janitor)
+library(cowplot)
+library(latex2exp)
+
+# Interpolation scenarios:
+# (1) Local-EC_Lakesmall = Cluster_strat75_holdout and
+# (2) Regional-EC_Regionsmall = Hu4_strat75_holdout
+
+# Then Extrapolation scenarios:
+# (1) Local-EC_Lakemoderate = Cluster_random50_holdout
+# (2) Random_Regionmoderate = Hu4_random50_holdout
+# (3) Regional-LU_Regionlarge = Hu4_ag50_holdout
+
+label_key <- data.frame(
+  set = factor(c(
+    "hu4_ago", "hu4_strat",
+    "hu4_random", "cluster_strat75",
+    "cluster_random50"
+  ), levels = c(
+    "cluster_strat75", "hu4_strat",
+    "cluster_random50", "hu4_random",
+    "hu4_ago"
+  )),
+  set_parsed = factor(c(
+    "B-Regional-LU Region_{large}",
+    "SR-Regional-EC Region_{small}", "B-Random Region_{moderate}", "SR-Local-EC Lake_{small}", "B-Local-EC Lake_{moderate}"
+  ), levels = c("SR-Local-EC Lake_{small}", "SR-Regional-EC Region_{small}", "B-Local-EC Lake_{moderate}", "B-Random Region_{moderate}", "B-Regional-LU Region_{large}")),
+  stringsAsFactors = FALSE
+)
 
 bar_plot_clean <- function(raw){
   random_lines <- raw %>%
@@ -11,9 +39,7 @@ bar_plot_clean <- function(raw){
 
   clean <- raw %>%
     filter(!(set %in% c("random25", "random75"))) %>%
-    mutate(set = factor(set, levels = c("cluster_strat75", "hu4_strat",
-                                        "cluster_random50", "hu4_random",
-                                        "hu4_ago"))) %>%
+    left_join(label_key, by = "set") %>%
     left_join(random_lines) %>%
     mutate(variable = factor(variable, levels = c("TP", "TN", "Chla", "secchi"))) %>%
     mutate(polation = case_when(set %in% c("cluster_strat75", "hu4_strat") ~ "interpolation",
@@ -32,15 +58,17 @@ raw <- tidyr::gather(raw, key = set, value = value, -variable)
 clean <- bar_plot_clean(raw)
 
 (gg_rmse <- ggplot(data = clean) +
-  geom_segment(aes(xend = set, yend = 0, x = set, y = value, color = polation),
+  geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
+                   y = value, color = polation),
                lineend = "butt", size = 8) +
   geom_hline(aes(yintercept = random25), linetype = "dashed") +
   geom_hline(aes(yintercept = random75)) +
   facet_wrap(~variable) +
   scale_color_manual(values = c("royalblue", "orange")) +
+  scale_x_discrete(labels = parse(text = TeX(levels(clean$set_parsed)))) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90),
-        legend.title=element_blank()) +
+        legend.title = element_blank()) +
   ylab("RMSE") + xlab(""))
 
 # ---- median_errors ----
@@ -60,17 +88,34 @@ raw           <- readxl::read_excel(
 
 clean <- bar_plot_clean(raw)
 
-gg_mrae <- ggplot(data = clean) +
-  geom_segment(aes(xend = set, yend = 0, x = set, y = value, color = polation),
-               lineend = "butt", size = 8) +
-  geom_hline(aes(yintercept = random25), linetype = "dashed") +
-  geom_hline(aes(yintercept = random75)) +
-  facet_wrap(~variable, scales = "free") +
-  scale_color_manual(values = c("royalblue", "orange")) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90),
-        legend.title=element_blank()) +
-  ylab("MRAE") + xlab("")
+(gg_mrae <- plot_grid(
+    ggplot(data = dplyr::filter(clean, variable %in% c("TP", "TN"))) +
+      geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
+                       y = value, color = polation),
+                   lineend = "butt", size = 8) +
+      geom_hline(aes(yintercept = random25), linetype = "dashed") +
+      geom_hline(aes(yintercept = random75)) +
+      facet_wrap(~variable, scales = "free", labeller = label_value) +
+      scale_color_manual(values = c("royalblue", "orange")) +
+      theme_minimal() +
+      theme(axis.text.x = element_blank(),
+            legend.title=element_blank()) +
+      ylab("MRAE") + xlab(""),
+    ggplot(data = dplyr::filter(clean, !(variable %in% c("TP", "TN")))) +
+      geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
+                       y = value, color = polation),
+                   lineend = "butt", size = 8) +
+      geom_hline(aes(yintercept = random25), linetype = "dashed") +
+      geom_hline(aes(yintercept = random75)) +
+      facet_wrap(~variable, scales = "free", labeller = label_value) +
+      scale_color_manual(values = c("royalblue", "orange")) +
+      scale_x_discrete(labels = parse(text = TeX(levels(clean$set_parsed)))) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 90),
+            legend.title=element_blank()) +
+      ylab("MRAE") + xlab(""),
+    ncol = 1, rel_heights = c(0.55, 1)))
+
 
 # ---- r2 ----
 
@@ -91,11 +136,12 @@ clean$x    <- rep(seq(0.5, 4), each = 5)
 clean$xend <- rep(seq(1.5, 5), each = 5)
 
 (gg_r2 <- ggplot(data = clean, aes(variable, value)) +
-  geom_bar(aes(fill = set), stat = "identity", position = position_dodge2()) +
+  geom_bar(aes(fill = set_parsed), stat = "identity", position = position_dodge2()) +
   geom_segment(aes(x = x, xend = xend, y = random25, yend = random25),
                linetype = "dashed") +
   geom_segment(aes(x = x, xend = xend, y = random75, yend = random75)) +
-  scale_fill_manual(values = c("orange", "#f4b183", "royalblue", "#9dc3e6", "#bdd7ee")) +
+  scale_fill_manual(values = c("orange", "#f4b183", "royalblue", "#9dc3e6", "#bdd7ee"),
+                    labels = parse(text = TeX(levels(clean$set_parsed)))) +
   theme_minimal() +
   theme(legend.title=element_blank()) +
   ylab(expression(R^{2})) + xlab(""))
@@ -108,11 +154,3 @@ ggsave("graphics/r2_bar.png", gg_r2)
 pdf("graphics/bar_plots.pdf")
 invisible(lapply(list(gg_rmse, gg_mrae, gg_r2), print))
 dev.off()
-
-# Interpolation scenarios:
-#   (1) Local-EC_Lakesmall = Cluster_strat75_holdout and
-# (2) Regional-EC_Regionsmall = Hu4_strat75_holdout
-# Then Extrapolation scenarios:
-#   (1) Local-EC_Lakemoderate = Cluster_random50_holdout
-# (2) Random_Regionmoderate = Hu4_random50_holdout
-# (3) Regional-LU_Regionlarge = Hu4_ag50_holdout
