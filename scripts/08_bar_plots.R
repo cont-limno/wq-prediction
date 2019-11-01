065a29d32764ee322eff898a5298f4793ad45959
jsta <stachel2@msu.edu>
2019-11-01 12:24:18 -0400

    add error bars to bar plots

--- scripts/08_bar_plots.R
+++ scripts/08_bar_plots.R
@@ -22,12 +22,13 @@ View(fonttable())
 # (3) Regional-LU_Regionlarge = Hu4_ag50_holdout : Targeted-AgRegion
 
 theme_opts <- theme(#axis.text.x = element_text(angle = 45, hjust = 0.98),
-                    axis.text.x = element_text(angle = 90, family = "Liberation Mono"),
+                    axis.text.x = element_text(angle = 90,
+                    family = "Liberation Mono"),
                     legend.title = element_blank(),
                     panel.border = element_rect(fill = "transparent", size = 1.2),
                     legend.position = "none")
 
-bar_size <- 4.8
+bar_size      <- 4.8
 facet_spacing <- 1.85
 
 (color_d <- c(
@@ -58,64 +59,6 @@ label_key <- data.frame(
   set_parsed = factor(label_names, levels = label_names),
   stringsAsFactors = FALSE
 )
-
-bar_plot_clean <- function(raw){
-  random_lines <- raw %>%
-    tidyr::spread(set, value)
-
-  clean <- raw %>%
-    dplyr::filter(!(variable %in% "secchi")) %>%
-    left_join(label_key, by = "set") %>%
-    left_join(random_lines) %>%
-    mutate(variable = factor(variable, levels = c("TP", "TN", "CHL", "secchi"))) %>%
-    mutate(polation = case_when(
-      set %in% c("random25", "random75",
-                 "cluster_strat75", "hu4_strat") ~ "interpolation",
-      TRUE ~ "extrapolation"))
-
-  clean
-}
-
-# ---- rmse ----
-raw           <- readxl::read_excel(
-  "data/bar_plot_data/2019.03.08LPEP2-RMSE-results_dec2018.xlsx",
-                          sheet = "conditional-combined plot")
-names(raw)[1] <- "variable"
-raw <- tidyr::gather(raw, key = set, value = value, -variable) %>%
-  mutate(variable = case_when(variable %in% c("Chla") ~ "CHL",
-                              TRUE ~ variable))
-
-clean <- bar_plot_clean(raw)
-
-(gg_rmse <- ggplot(data = clean) +
-  geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
-                   y = value, color = set_parsed),
-               lineend = "butt", size = bar_size) +
-    ylim(0, 1) +
-  facet_wrap(~variable) +
-  scale_color_manual(values = color_d,
-                     labels = parse(text = TeX(levels(clean$set_parsed)))) +
-  theme_minimal() +
-    theme_opts +
-    theme(panel.spacing.x = unit(facet_spacing, "lines")) +
-  ylab("RMSE") + xlab(""))
-
-# ---- median_errors ----
-
-raw           <- readxl::read_excel(
-  "data/bar_plot_data/median_error_03082019Qi.xlsx",
-  sheet = "conditional")[,1:4] %>%
-  setNames(c("variable", "set", "error_relative", "error_nonrelative")) %>%
-  janitor::remove_empty(which = "rows") %>%
-  tidyr::fill(variable, .direction = "down") %>%
-  select(-error_relative) %>%
-  rename(value = error_nonrelative) %>%
-  mutate(set = case_when(set %in% c("cluster_random50_holdout") ~ "cluster_random50",
-                              TRUE ~ set)) %>%
-  mutate(variable = case_when(variable %in% c("chla") ~ "CHL",
-                             TRUE ~ variable))
-
-clean <- bar_plot_clean(raw)
 
 zero_pad <- function(x, width = 3){
   sapply(x, function(y){
@@ -159,10 +102,70 @@ my_breaks <- function(x) {
   }
 }
 
+bar_plot_clean <- function(x){
+  x <- x %>%
+    mutate(variable = case_when(variable %in% c("chla") ~ "CHL",
+                                TRUE ~ variable),
+           set = case_when(set == "cluster_random50_holdout" ~ "cluster_random50",
+                           TRUE ~ set)) %>%
+
+    arrange(set) %>%
+    dplyr::select(variable, set, value, stdev)
+
+  clean <- x %>%
+    dplyr::filter(!(variable %in% "secchi")) %>%
+    left_join(label_key, by = "set") %>%
+    # left_join(random_lines) %>%
+    mutate(variable = factor(variable, levels = c("TP", "TN", "CHL", "secchi"))) %>%
+    mutate(polation = case_when(
+      set %in% c("random25", "random75",
+                 "cluster_strat75", "hu4_strat") ~ "interpolation",
+      TRUE ~ "extrapolation"))
+
+  clean
+}
+
+# ---- rmse ----
+raw <- read.csv("data/revision_datasets/mean_variance_statistics/MeanPerformanceAcross10.csv",
+                 stringsAsFactors = FALSE) %>%
+  janitor::clean_names() %>%
+  rename(variable = target, set = splitting, value = avg_rmse) %>%
+  mutate(stdev = replace_na(sqrt(variance_rmse), 0))
+
+clean <- bar_plot_clean(raw) %>%
+  mutate(ymin = value - (stdev / sqrt(10)), # we ran 10 replicate models
+         ymax = value + (stdev / sqrt(10)))
+
+(gg_rmse <-  ggplot(data = clean) +
+  geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
+                   y = value, color = set_parsed),
+               lineend = "butt", size = bar_size) +
+  geom_errorbar(aes(ymin = ymin, ymax = ymax, x = set_parsed)) +
+  ylim(0, 1) +
+  facet_wrap(~variable) +
+  scale_color_manual(values = color_d,
+                     labels = parse(text = TeX(levels(clean$set_parsed)))) +
+  theme_minimal() +
+    theme_opts +
+    theme(panel.spacing.x = unit(facet_spacing, "lines")) +
+  ylab("RMSE") + xlab(""))
+
+# ---- median_errors ----
+raw <- read.csv("data/revision_datasets/mean_variance_statistics/MeanPerformanceAcross10.csv",
+                stringsAsFactors = FALSE) %>%
+  janitor::clean_names() %>%
+  rename(variable = target, set = splitting, value = avg_median_error_abs) %>%
+  mutate(stdev = replace_na(sqrt(variance_median_error_abs), 0))
+
+clean <- bar_plot_clean(raw) %>%
+  mutate(ymin = value - (stdev / sqrt(10)), # we ran 10 replicate models
+         ymax = value + (stdev / sqrt(10)))
+
 (gg_mrae <- ggplot(data = clean) +
       geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
                        y = value, color = set_parsed),
                    lineend = "butt", size = bar_size) +
+    geom_errorbar(aes(ymin = ymin, ymax = ymax, x = set_parsed)) +
       scale_y_continuous(breaks = my_breaks,
                          labels = zero_pad) +
       facet_wrap(~variable, scales = "free", labeller = label_value) +
@@ -174,24 +177,21 @@ my_breaks <- function(x) {
       ylab("MRAE") + xlab(""))
 
 # ---- r2 ----
-raw           <- readxl::read_excel(
-  "data/bar_plot_data/R_squared_03072019Qi.xlsx",
-  sheet = "conditional")[1:35,1:3] %>%
-  setNames(c("variable", "set", "value")) %>%
-  mutate(value = as.numeric(value)) %>%
-  janitor::remove_empty(which = "rows") %>%
-  tidyr::fill(variable, .direction = "down") %>%
-  mutate(set = case_when(set %in% c("cluster_random50_holdout") ~ "cluster_random50",
-                         TRUE ~ set)) %>%
-  mutate(variable = case_when(variable %in% c("chla") ~ "CHL",
-                              TRUE ~ variable))
+raw <- read.csv("data/revision_datasets/mean_variance_statistics/MeanPerformanceAcross10.csv",
+                stringsAsFactors = FALSE) %>%
+  janitor::clean_names() %>%
+  rename(variable = target, set = splitting, value = avg_r2) %>%
+  mutate(stdev = replace_na(sqrt(variance_r2), 0))
 
-clean      <- bar_plot_clean(raw)
+clean <- bar_plot_clean(raw) %>%
+  mutate(ymin = value - (stdev / sqrt(10)), # we ran 10 replicate models
+         ymax = value + (stdev / sqrt(10)))
 
 (gg_r2 <- ggplot(data = clean) +
     geom_segment(aes(xend = set_parsed, yend = 0, x = set_parsed,
                      y = value, color = set_parsed),
                  lineend = "butt", size = bar_size) +
+    geom_errorbar(aes(ymin = ymin, ymax = ymax, x = set_parsed)) +
     scale_y_continuous(breaks = seq(0.10, 0.60, by = 0.10),
                        labels = zero_pad2) +
     facet_wrap(~variable, labeller = label_value) +
@@ -218,6 +218,6 @@ clean      <- bar_plot_clean(raw)
 ggsave("graphics/bar_plots.png", gg_all, height = 8)
 
 # https://stackoverflow.com/a/20502085/3362993
-pdf("graphics/bar_plots.pdf")
-invisible(lapply(list(gg_all), print))
-dev.off()
+# pdf("graphics/bar_plots.pdf")
+# invisible(lapply(list(gg_all), print))
+# dev.off()
