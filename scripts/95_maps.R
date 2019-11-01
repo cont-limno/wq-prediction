3bed908812ae0f31715be482561250583ed1b32e
immccull95 <imccullough@bren.ucsb.edu>
2019-11-01 09:43:15 -0400

    ian updated figs 1 and 4 and associated code

--- scripts/95_maps.R
+++ scripts/95_maps.R
@@ -1,6 +1,6 @@
 ################# LPEP2: Map figures ###########################################################
 # Date: 3-14-19
-# updated: 3-26-19
+# updated: 11-1-19
 # Author: Ian McCullough, immccull@gmail.com
 ################################################################################################
 
@@ -22,7 +22,8 @@ lakes_4ha_pts <- shapefile("C:/Ian_GIS/LAGOS-NE-GISv1.0/LAGOS_NE_All_Lakes_4ha_P
 HU4 <- shapefile("C:/Ian_GIS/LAGOS-NE-GISv1.0/HU4/HU4.shp")
 
 # water quality/ecological context for different model scenarios
-datatab <- read.csv("data/raw_data_LPEP2_03082019.csv")
+#datatab <- read.csv("data/raw_data_LPEP2_03082019.csv") #original submission
+datatab <- read.csv("data/revision_datasets/wq2_single_run1.csv")[,c(2:22)] #revision
 
 ########### Main program ##################
 ## Map of Secchi
@@ -73,31 +74,34 @@ dev.off()
 
 ## Maps of training/test datasets
 # get xy coordinates for mapping
-test_train_df <- as.data.frame(lakes_4ha_pts_secchi@data)
-test_train_df$xCor <- lakes_4ha_pts_secchi@coords[,1]
-test_train_df$yCor <- lakes_4ha_pts_secchi@coords[,2]
-test_train_df <- test_train_df[,c(1,36:42,65,66)] #remove unused columns; make dataframe more workable
+test_train_df <- as.data.frame(lakes_4ha_pts@data)
+test_train_df$xCor <- lakes_4ha_pts@coords[,1]
+test_train_df$yCor <- lakes_4ha_pts@coords[,2]
+test_train_df <- merge(test_train_df, datatab, by.x='lagoslakei', by.y='lagoslakeid', all.x=F)
+
+test_train_df <- test_train_df[,c(1,33,34,48:54)] #remove unused columns; make dataframe more workable
 
 # assign "Training" to 1, "Testing" to 0, then reorder factor levels
-test_train_df$random25_holdout <- as.factor(ifelse(test_train_df$random25_holdout==0, 'Training','Testing'))
+# Test=TRUE, TRAIN=FALSE
+test_train_df$random25_holdout <- as.factor(ifelse(test_train_df$random25_holdout==FALSE, 'Training','Testing'))
 #test_train_df$random25_holdout <- factor(test_train_df$random25_holdout,levels(test_train_df$random25_holdout)[c(2,1)])
 
-test_train_df$random75_holdout <- as.factor(ifelse(test_train_df$random75_holdout==0, 'Training','Testing'))
+test_train_df$random75_holdout <- as.factor(ifelse(test_train_df$random75_holdout==FALSE, 'Training','Testing'))
 #test_train_df$random75_holdout <- factor(test_train_df$random75_holdout,levels(test_train_df$random75_holdout)[c(2,1)])
 
-test_train_df$hu4_ag50_holdout <- as.factor(ifelse(test_train_df$hu4_ag50_holdout==0, 'Training','Testing'))
+test_train_df$hu4_ag50_holdout <- as.factor(ifelse(test_train_df$hu4_ag50_holdout==FALSE, 'Training','Testing'))
 #test_train_df$hu4_ag50_holdout <- factor(test_train_df$hu4_ag50_holdout,levels(test_train_df$hu4_ag50_holdout)[c(2,1)])
 
-test_train_df$hu4_strat75_holdout <- as.factor(ifelse(test_train_df$hu4_strat75_holdout==0, 'Training','Testing'))
+test_train_df$hu4_strat75_holdout <- as.factor(ifelse(test_train_df$hu4_strat75_holdout==FALSE, 'Training','Testing'))
 #test_train_df$hu4_strat75_holdout <- factor(test_train_df$hu4_strat75_holdout,levels(test_train_df$hu4_strat75_holdout)[c(2,1)])
 
-test_train_df$hu4_random50_holdout <- as.factor(ifelse(test_train_df$hu4_random50_holdout==0,'Training','Testing'))
+test_train_df$hu4_random50_holdout <- as.factor(ifelse(test_train_df$hu4_random50_holdout==FALSE,'Training','Testing'))
 #test_train_df$hu4_random50_holdout <- factor(test_train_df$hu4_random50_holdout,levels(test_train_df$hu4_random50_holdout)[c(2,1)])
 
-test_train_df$cluster_strat75_holdout <- as.factor(ifelse(test_train_df$cluster_strat75_holdout==0,'Training','Testing'))
+test_train_df$cluster_strat75_holdout <- as.factor(ifelse(test_train_df$cluster_strat75_holdout==FALSE,'Training','Testing'))
 #test_train_df$cluster_strat75_holdout <- factor(test_train_df$cluster_strat75_holdout,levels(test_train_df$cluster_strat75_holdout)[c(2,1)])
 
-test_train_df$cluster_random50_holdout <- as.factor(ifelse(test_train_df$cluster_random50_holdout==0,'Training','Testing'))
+test_train_df$cluster_random50_holdout <- as.factor(ifelse(test_train_df$cluster_random50_holdout==FALSE,'Training','Testing'))
 #test_train_df$cluster_random50_holdout <- factor(test_train_df$cluster_random50_holdout,levels(test_train_df$cluster_random50_holdout)[c(2,1)])
 
 # random25_holdout
@@ -105,7 +109,7 @@ mappoint_size <- 0.15
 title_size <- 9
 random25_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$random25_holdout), size=mappoint_size) +
-  ggtitle('a) Random_Lakelarge')+
+  #ggtitle('a) Random_Lakelarge')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -126,7 +130,7 @@ random25_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # random75_holdout
 random75_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$random75_holdout), size=mappoint_size) +
-  ggtitle('b) Random_Lakesmall')+
+  #ggtitle('b) Random_Lakesmall')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -147,7 +151,7 @@ random75_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # cluster_strat75_holdout
 cluster_strat75_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$cluster_strat75_holdout), size=mappoint_size) +
-  ggtitle('c) Local-EC_Lakesmall')+
+  #ggtitle('c) Local-EC_Lakesmall')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -168,7 +172,7 @@ cluster_strat75_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # hu4_strat75_holdout
 hu4_strat75_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$hu4_strat75_holdout), size=mappoint_size) +
-  ggtitle('d) Regional-EC_Regionsmall')+
+  #ggtitle('d) Regional-EC_Regionsmall')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -189,7 +193,7 @@ hu4_strat75_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # cluster_random50_holdout
 cluster_random50_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$cluster_random50_holdout), size=mappoint_size) +
-  ggtitle('e) Local-EC_Lakemoderate')+
+  #ggtitle('e) Local-EC_Lakemoderate')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -210,7 +214,7 @@ cluster_random50_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # hu4_random50_holdout
 hu4_random50_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$hu4_random50_holdout), size=mappoint_size) +
-  ggtitle('f) Random_Regionmoderate')+
+  #ggtitle('f) Random_Regionmoderate')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -231,7 +235,7 @@ hu4_random50_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # hu4_ag50_holdout
 hu4_ag50_holdout.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$hu4_ag50_holdout), size=mappoint_size) +
-  ggtitle('g) Regional-LU_Regionlarge')+
+  #ggtitle('g) Regional-LU_Regionlarge')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Training','Testing'),
@@ -270,7 +274,7 @@ blank.plot <- ggplot(test_train_df, aes(x=xCor,y=yCor))+
 # legend plot (created so can extract legend from it; this plot not plotted)
 legend.point1<-ggplot(test_train_df, aes(x=xCor,y=yCor))+
   geom_point(aes(colour=test_train_df$random25_holdout), size=mappoint_size) +
-  ggtitle('Random25_holdout')+
+  #ggtitle('Random25_holdout')+
   geom_path(data=HU4,aes(long,lat,group=group),colour='black', size=0.2) + coord_equal()+
   scale_color_manual(values=c("orange", "royalblue"),
                      labels=c('Testing','Training'),
@@ -298,7 +302,7 @@ legend <- g_legend(legend.point1)
 #grid.draw(legend) 
 
 ## create multi-panel plot (warning, takes a couple mins)
-jpeg('graphics/train_test_map.jpeg',width = 6,height = 5,units = 'in',res=600)
+jpeg('graphics/train_test_map_revised.jpeg',width = 6,height = 5,units = 'in',res=600)
   # plotting positions!
   # (upper left, upper mid, uppper right,
   # mid left, mid mid, mid right,
--- scripts/97_big_density_plot.R
+++ scripts/97_big_density_plot.R
@@ -1,6 +1,6 @@
 ################# LPEP2: Density plots ##########################################################
 # Date: 7-1-19 (Happy Canada Day)
-# updated:
+# updated: 11-1-19
 # Author: Ian McCullough, immccull@gmail.com
 ################################################################################################
 
@@ -13,36 +13,43 @@ library(grid)
 setwd('C:/Users/FWL/Documents/wq-prediction')
 
 # water quality/ecological context for different model scenarios
-datatab <- read.csv("data/raw_data_LPEP2_03082019.csv")
-
+#datatab <- read.csv("data/raw_data_LPEP2_03082019.csv") #original submission
+datatab <- read.csv("data/revision_datasets/wq2_single_run1.csv")[,c(2:22)] #revision
+local_predictors <- read.csv("data/local_predictors.csv") #for max depth and % forest variables
 
 ################## Main program #################
-datatab <- datatab[,c(1,5:16,18)] #keep only some columns
+datatab <- datatab[,c(1:4,11,15:21)] #keep only some columns
+local_predictors <- local_predictors[,c('lagoslakeid','maxdepth_m','iws_nlcd2006_for')]
+datatab <- merge(datatab, local_predictors, by='lagoslakeid', all.x=T) #merge in maxdepth and forest variables
 
+# Test=TRUE, TRAIN=FALSE
 # assign "Training" to 1, "Testing" to 0, then reorder factor levels
-datatab$random25_holdout <- as.factor(ifelse(datatab$random25_holdout==1, 'Testing','Training'))
+datatab$random25_holdout <- as.factor(ifelse(datatab$random25_holdout==TRUE, 'Testing','Training'))
 datatab$random25_holdout <- factor(datatab$random25_holdout,levels(datatab$random25_holdout)[c(2,1)])
 
-datatab$random75_holdout <- as.factor(ifelse(datatab$random75_holdout==1, 'Testing','Training'))
+datatab$random75_holdout <- as.factor(ifelse(datatab$random75_holdout==TRUE, 'Testing','Training'))
 datatab$random75_holdout <- factor(datatab$random75_holdout,levels(datatab$random75_holdout)[c(2,1)])
 
-datatab$hu4_ag50_holdout <- as.factor(ifelse(datatab$hu4_ag50_holdout==1, 'Testing','Training'))
+datatab$hu4_ag50_holdout <- as.factor(ifelse(datatab$hu4_ag50_holdout==TRUE, 'Testing','Training'))
 datatab$hu4_ag50_holdout <- factor(datatab$hu4_ag50_holdout,levels(datatab$hu4_ag50_holdout)[c(2,1)])
 
-datatab$hu4_strat75_holdout <- as.factor(ifelse(datatab$hu4_strat75_holdout==1, 'Testing','Training'))
+datatab$hu4_strat75_holdout <- as.factor(ifelse(datatab$hu4_strat75_holdout==TRUE, 'Testing','Training'))
 datatab$hu4_strat75_holdout <- factor(datatab$hu4_strat75_holdout,levels(datatab$hu4_strat75_holdout)[c(2,1)])
 
-datatab$hu4_random50_holdout <- as.factor(ifelse(datatab$hu4_random50_holdout==1,'Testing','Training'))
+datatab$hu4_random50_holdout <- as.factor(ifelse(datatab$hu4_random50_holdout==TRUE,'Testing','Training'))
 datatab$hu4_random50_holdout <- factor(datatab$hu4_random50_holdout,levels(datatab$hu4_random50_holdout)[c(2,1)])
 
-datatab$cluster_strat75_holdout <- as.factor(ifelse(datatab$cluster_strat75_holdout==1,'Testing','Training'))
+datatab$cluster_strat75_holdout <- as.factor(ifelse(datatab$cluster_strat75_holdout==TRUE,'Testing','Training'))
 datatab$cluster_strat75_holdout <- factor(datatab$cluster_strat75_holdout,levels(datatab$cluster_strat75_holdout)[c(2,1)])
 
-datatab$cluster_random50_holdout <- as.factor(ifelse(datatab$cluster_random50_holdout==1,'Testing','Training'))
+datatab$cluster_random50_holdout <- as.factor(ifelse(datatab$cluster_random50_holdout==TRUE,'Testing','Training'))
 datatab$cluster_random50_holdout <- factor(datatab$cluster_random50_holdout,levels(datatab$cluster_random50_holdout)[c(2,1)])
 
 plot_colorz <- c('royalblue','orange')
 title_size = 7
+
+# create separate dataframe for 'plot E' (cluster random 50 holdout; NAs need to be removed; otherwise messes up plots)
+plotE_data <- subset(datatab, cluster_random50_holdout=='Training' | cluster_random50_holdout=='Testing')
 
 #### TP ####
 TP_xlims <- c(0,200)
@@ -119,7 +126,7 @@ plotD_TP <- ggplot(datatab, aes(x=tp, fill=factor(hu4_strat75_holdout))) + geom_
         plot.title=element_blank())
 
 # cluster_random50_holdout
-plotE_TP <- ggplot(datatab, aes(x=tp, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
+plotE_TP <- ggplot(plotE_data, aes(x=tp, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TP_xlims)+
   scale_y_continuous(limits=TP_ylims)+
@@ -213,7 +220,7 @@ variable_label <- grobTree(textGrob("CHL", x=0.8,  y=0.9, hjust=0,
                                     gp=gpar(col="black", fontsize=title_size)))
 
 # random25_holdout
-plotA_chla <- ggplot(datatab, aes(x=chal, fill=factor(random25_holdout))) + geom_density(alpha=0.75)+
+plotA_chla <- ggplot(datatab, aes(x=chla, fill=factor(random25_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -230,7 +237,7 @@ plotA_chla <- ggplot(datatab, aes(x=chal, fill=factor(random25_holdout))) + geom
         plot.title=element_blank())
 
 # random75_holdout
-plotB_chla <- ggplot(datatab, aes(x=chal, fill=factor(random75_holdout))) + geom_density(alpha=0.75)+
+plotB_chla <- ggplot(datatab, aes(x=chla, fill=factor(random75_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -247,7 +254,7 @@ plotB_chla <- ggplot(datatab, aes(x=chal, fill=factor(random75_holdout))) + geom
         plot.title=element_blank())
 
 # cluster_strat75_holdout
-plotC_chla <- ggplot(datatab, aes(x=chal, fill=factor(cluster_strat75_holdout))) + geom_density(alpha=0.75)+
+plotC_chla <- ggplot(datatab, aes(x=chla, fill=factor(cluster_strat75_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -264,7 +271,7 @@ plotC_chla <- ggplot(datatab, aes(x=chal, fill=factor(cluster_strat75_holdout)))
         plot.title=element_blank())
 
 # hu4_strat75_holdout
-plotD_chla <- ggplot(datatab, aes(x=chal, fill=factor(hu4_strat75_holdout))) + geom_density(alpha=0.75)+
+plotD_chla <- ggplot(datatab, aes(x=chla, fill=factor(hu4_strat75_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -281,7 +288,7 @@ plotD_chla <- ggplot(datatab, aes(x=chal, fill=factor(hu4_strat75_holdout))) + g
         plot.title=element_blank())
 
 # cluster_random50_holdout
-plotE_chla <- ggplot(datatab, aes(x=chal, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
+plotE_chla <- ggplot(plotE_data, aes(x=chla, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -298,7 +305,7 @@ plotE_chla <- ggplot(datatab, aes(x=chal, fill=factor(cluster_random50_holdout))
         plot.title=element_blank())
 
 # hu4_random50_holdout
-plotF_chla <- ggplot(datatab, aes(x=chal, fill=factor(hu4_random50_holdout))) + geom_density(alpha=0.75)+
+plotF_chla <- ggplot(datatab, aes(x=chla, fill=factor(hu4_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -315,7 +322,7 @@ plotF_chla <- ggplot(datatab, aes(x=chal, fill=factor(hu4_random50_holdout))) + 
         plot.title=element_blank())
 
 # hu4_ag50_holdout
-plotG_chla <- ggplot(datatab, aes(x=chal, fill=factor(hu4_ag50_holdout))) + geom_density(alpha=0.75)+
+plotG_chla <- ggplot(datatab, aes(x=chla, fill=factor(hu4_ag50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=chla_xlims)+
   scale_y_continuous(limits=chla_ylims)+
@@ -338,7 +345,7 @@ variable_label <- grobTree(textGrob("TN", x=0.8,  y=0.9, hjust=0,
                                     gp=gpar(col="black", fontsize=title_size)))
 
 # random25_holdout
-plotA_TN <- ggplot(datatab, aes(x=tn, fill=factor(random25_holdout))) + geom_density(alpha=0.75)+
+plotA_TN <- ggplot(datatab, aes(x=tn_combined, fill=factor(random25_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -355,7 +362,7 @@ plotA_TN <- ggplot(datatab, aes(x=tn, fill=factor(random25_holdout))) + geom_den
         plot.title=element_blank())
 
 # random75_holdout
-plotB_TN <- ggplot(datatab, aes(x=tn, fill=factor(random75_holdout))) + geom_density(alpha=0.75)+
+plotB_TN <- ggplot(datatab, aes(x=tn_combined, fill=factor(random75_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -372,7 +379,7 @@ plotB_TN <- ggplot(datatab, aes(x=tn, fill=factor(random75_holdout))) + geom_den
         plot.title=element_blank())
 
 # cluster_strat75_holdout
-plotC_TN <- ggplot(datatab, aes(x=tn, fill=factor(cluster_strat75_holdout))) + geom_density(alpha=0.75)+
+plotC_TN <- ggplot(datatab, aes(x=tn_combined, fill=factor(cluster_strat75_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -389,7 +396,7 @@ plotC_TN <- ggplot(datatab, aes(x=tn, fill=factor(cluster_strat75_holdout))) + g
         plot.title=element_blank())
 
 # hu4_strat75_holdout
-plotD_TN <- ggplot(datatab, aes(x=tn, fill=factor(hu4_strat75_holdout))) + geom_density(alpha=0.75)+
+plotD_TN <- ggplot(datatab, aes(x=tn_combined, fill=factor(hu4_strat75_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -406,7 +413,7 @@ plotD_TN <- ggplot(datatab, aes(x=tn, fill=factor(hu4_strat75_holdout))) + geom_
         plot.title=element_blank())
 
 # cluster_random50_holdout
-plotE_TN <- ggplot(datatab, aes(x=tn, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
+plotE_TN <- ggplot(plotE_data, aes(x=tn_combined, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -423,7 +430,7 @@ plotE_TN <- ggplot(datatab, aes(x=tn, fill=factor(cluster_random50_holdout))) + 
         plot.title=element_blank())
 
 # hu4_random50_holdout
-plotF_TN <- ggplot(datatab, aes(x=tn, fill=factor(hu4_random50_holdout))) + geom_density(alpha=0.75)+
+plotF_TN <- ggplot(datatab, aes(x=tn_combined, fill=factor(hu4_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -440,7 +447,7 @@ plotF_TN <- ggplot(datatab, aes(x=tn, fill=factor(hu4_random50_holdout))) + geom
         plot.title=element_blank())
 
 # hu4_ag50_holdout
-plotG_TN <- ggplot(datatab, aes(x=tn, fill=factor(hu4_ag50_holdout))) + geom_density(alpha=0.75)+
+plotG_TN <- ggplot(datatab, aes(x=tn_combined, fill=factor(hu4_ag50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=TN_xlims)+
   scale_y_continuous(limits=TN_ylims)+
@@ -531,7 +538,7 @@ plotD_Secchi <- ggplot(datatab, aes(x=secchi, fill=factor(hu4_strat75_holdout)))
         plot.title=element_blank())
 
 # cluster_random50_holdout
-plotE_Secchi <- ggplot(datatab, aes(x=secchi, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
+plotE_Secchi <- ggplot(plotE_data, aes(x=secchi, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=secchi_xlims)+
   scale_y_continuous(limits=secchi_ylims)+
@@ -657,7 +664,7 @@ plotD_depth <- ggplot(datatab, aes(x=maxdepth_m, fill=factor(hu4_strat75_holdout
         plot.title=element_blank())
 
 # cluster_random50_holdout
-plotE_depth <- ggplot(datatab, aes(x=maxdepth_m, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
+plotE_depth <- ggplot(plotE_data, aes(x=maxdepth_m, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=depth_xlims)+
   scale_y_continuous(limits=depth_ylims)+
@@ -782,7 +789,7 @@ plotD_forest <- ggplot(datatab, aes(x=iws_nlcd2006_for, fill=factor(hu4_strat75_
         plot.title=element_blank())
 
 # cluster_random50_holdout
-plotE_forest <- ggplot(datatab, aes(x=iws_nlcd2006_for, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
+plotE_forest <- ggplot(plotE_data, aes(x=iws_nlcd2006_for, fill=factor(cluster_random50_holdout))) + geom_density(alpha=0.75)+
   theme_bw()+
   scale_x_continuous(limits=forest_xlims)+
   scale_y_continuous(limits=forest_ylims)+
@@ -832,7 +839,7 @@ plotG_forest <- ggplot(datatab, aes(x=iws_nlcd2006_for, fill=factor(hu4_ag50_hol
         plot.margin=unit(c(0.05,0.05,0.05,0.05), "cm"),
         plot.title=element_blank())
 
-jpeg('graphics/density_plots/all_vars_scenarios.jpeg',width = 6.5,height = 6.5,units = 'in',res=600)
+jpeg('graphics/density_plots/all_vars_scenarios_revised.jpeg',width = 6.5,height = 6.5,units = 'in',res=600)
 grid.arrange(plotA_TP, plotA_TN, plotA_chla, plotA_Secchi, plotA_depth, plotA_forest,
              plotB_TP, plotB_TN, plotB_chla, plotB_Secchi, plotB_depth, plotB_forest,
              plotC_TP, plotC_TN, plotC_chla, plotC_Secchi, plotC_depth, plotC_forest,
